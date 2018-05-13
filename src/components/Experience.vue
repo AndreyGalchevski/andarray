d162a43ebee1abbedd78b57de4a08cde47ece767
Andrey Galchevski <andreygalchevski@gmail.com>
2018-05-13 21:40:14 +0300

    added technology logos to portfolio cards, started to work on positions cards

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -1,26 +1,58 @@
 <template>
 	<div class="experience section" id="experience-section">
 		<h1>Experience</h1>
-        <div class="section">
-			<i class="flow-text fas fa-database"></i>
-            <p class="flow-text">Data Analyst/Statistician</p>
-        </div>
-        <div class="section">
-			<i class="flow-text fas fa-male"></i>
-            <p class="flow-text">Team Lead</p>
-        </div>
-        <div class="section">
-			<i class="flow-text fas fa-code"></i>
-            <p class="flow-text">Full-Stack Dev</p>
-        </div>
+		<div class="row">
+			<div class="col s12 m4" v-for="position in positions" :key="position.name">
+				<div class="card">
+					<div class="card-image waves-effect waves-block waves-light">
+						<img 
+							class="activator position-img" 
+							:src="position.imgSrc"
+							:alt="position.imgAlt">
+					</div>
+					<div class="card-content">
+						<span class="card-title activator grey-text text-darken-4">{{position.name}}
+							<i class="material-icons right">more_vert</i>
+						</span>
+					</div>
+					<div class="card-reveal">
+						<span class="card-title grey-text text-darken-4">{{position.name}}
+							<i class="material-icons right">close</i>
+						</span>
+					<p>{{position.description}}</p>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script>
 export default {
 	name: 'Experience',
-	props: {
-		msg: String
+	data () {
+		return {
+			positions: [
+				{
+					name: 'Data Analyst',
+					description: 'bla bla bla data analysis bla bla bla numbers bla bla bla distribution',
+					imgSrc: 'https://res.cloudinary.com/dqvimfd8b/image/upload/v1526234576/andarray/experience/data.png',
+					imgAlt: 'Data analysis image'
+				},
+				{
+					name: 'Team Lead',
+					description: 'bla bla bla data agile bla bla bla HR bla bla bla dead lines',
+					imgSrc: 'https://res.cloudinary.com/dqvimfd8b/image/upload/v1526234576/andarray/experience/team_lead.jpg',
+					imgAlt: 'Busy man doing a bunch of stuff'
+				},
+				{
+					name: 'Full-Stack Dev',
+					description: 'bla bla bla data algorithms bla bla bla frameworks bla bla bla semicolons',
+					imgSrc: 'https://res.cloudinary.com/dqvimfd8b/image/upload/v1526234576/andarray/experience/developer.jpg',
+					imgAlt: 'Work table with a laptop, coffee, pencils etc'
+				}
+			]
+		}
 	}
 }
 </script>
@@ -31,5 +63,9 @@ export default {
 	position: relative;
 	background-color: white;
 	z-index: 1;
+}
+.position-img {
+	width: 160px;
+	height: 200px;
 }
 </style>
--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -17,6 +17,12 @@
 					</div>
                     <div class="card-content">
                         <p class="flow-text">{{project.name}}</p>
+						<span v-for="tech in project.techs" :key="tech.name">
+							<img 
+								class="tech-logo" 
+								:src="tech.logoSrc"
+								:alt="tech.logoAlt" />
+						</span>
                     </div>
                 </div>
             </div>
@@ -34,19 +40,85 @@ export default {
 					name: 'Total Liberation',
 					imgSrc: 'https://res.cloudinary.com/dqvimfd8b/image/upload/v1524496785/andarray/portfolio/totalliberation-website.png',
 					imgAlt: 'Total Liberation Israel website screenshot',
-					link: 'https://www.totalliberationisrael.com'
+					link: 'https://www.totalliberationisrael.com',
+					techs: [
+						{
+							name: 'Bootstrap',
+							logoSrc: 'https://res.cloudinary.com/dqvimfd8b/image/upload/v1526221581/andarray/tech/bootstrap.png',
+							logoAlt: 'Bootstrap CSS framework logo image'
+						},
+						{
+							name: 'Vue',
+							logoSrc: 'https://res.cloudinary.com/dqvimfd8b/image/upload/v1526221582/andarray/tech/vue.png',
+							logoAlt: 'VueJS logo image'
+						},
+						{
+							name: 'Express',
+							logoSrc: 'https://res.cloudinary.com/dqvimfd8b/image/upload/v1526221581/andarray/tech/express.png',
+							logoAlt: 'ExpressJS logo image'
+						},
+						{
+							name: 'Node',
+							logoSrc: 'https://res.cloudinary.com/dqvimfd8b/image/upload/v1526221582/andarray/tech/node.png',
+							logoAlt: 'NodeJS logo image'
+						},
+						{
+							name: 'Mongo',
+							logoSrc: 'https://res.cloudinary.com/dqvimfd8b/image/upload/v1526221582/andarray/tech/mongo.png',
+							logoAlt: 'MongoDB logo image'
+						}
+					]
 				},
 				{
 					name: 'VeganFuel',
 					imgSrc: 'https://res.cloudinary.com/dqvimfd8b/image/upload/v1524496784/andarray/portfolio/veganfuel-website.png',
 					imgAlt: 'VeganFuel app screenshot from Google Play website',
-					link: 'https://play.google.com/store/apps/details?id=andreygalchevski.veganfuel&amp;hl=en_GB'
+					link: 'https://play.google.com/store/apps/details?id=andreygalchevski.veganfuel&amp;hl=en_GB',
+					techs: [
+						{
+							name: 'Ionic',
+							logoSrc: 'https://res.cloudinary.com/dqvimfd8b/image/upload/v1526221581/andarray/tech/ionic.png',
+							logoAlt: 'Ionic 2 logo image'
+						},
+						{
+							name: 'Angular',
+							logoSrc: 'https://res.cloudinary.com/dqvimfd8b/image/upload/v1526221581/andarray/tech/angular.png',
+							logoAlt: 'Angular logo image'
+						},
+						{
+							name: 'Cordova',
+							logoSrc: 'https://res.cloudinary.com/dqvimfd8b/image/upload/v1526221581/andarray/tech/cordova.png',
+							logoAlt: 'Cordova logo image'
+						}
+					]
 				},
 				{
 					name: 'Insapient',
 					imgSrc: 'https://res.cloudinary.com/dqvimfd8b/image/upload/v1524496783/andarray/portfolio/insapient-website.png',
 					imgAlt: 'Insapient band website screenshot',
-					link: 'https://www.google.com'
+					link: 'https://www.google.com',
+					techs: [
+						{
+							name: 'Materialize',
+							logoSrc: 'https://res.cloudinary.com/dqvimfd8b/image/upload/v1526221582/andarray/tech/materialize.png',
+							logoAlt: 'Materialize CSS framework logo image'
+						},
+						{
+							name: 'HTML',
+							logoSrc: 'https://res.cloudinary.com/dqvimfd8b/image/upload/v1526233059/andarray/tech/html.png',
+							logoAlt: 'HTML logo image'
+						},
+						{
+							name: 'CSS',
+							logoSrc: 'https://res.cloudinary.com/dqvimfd8b/image/upload/v1526233060/andarray/tech/css.png',
+							logoAlt: 'CSS logo image'
+						},
+						{
+							name: 'JS',
+							logoSrc: 'https://res.cloudinary.com/dqvimfd8b/image/upload/v1526233060/andarray/tech/js.png',
+							logoAlt: 'JavaScript logo image'
+						}
+					]
 				}
 			]
 		}
@@ -60,5 +132,12 @@ export default {
 	position: relative;
 	background-color: white;
 	z-index: 1;
+}
+.tech-logo {
+	-webkit-align-content: center;
+	align-content: center;
+	width: 5vh;
+	height: 5vh;
+	margin: 1vh;
 }
 </style>
